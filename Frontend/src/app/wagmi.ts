2d41fa0c3fac84c2e4e4fa42bfd0b518c4e7bd23
Aidan Costello <apc5074@g.rit.edu>
2024-10-26 17:57:46 -0400

    changed to metamask

--- Frontend/src/app/wagmi.ts
+++ Frontend/src/app/wagmi.ts
@@ -1,17 +1,12 @@
 import { http, createConfig } from "wagmi";
 import { baseSepolia } from "wagmi/chains";
-import { coinbaseWallet } from "wagmi/connectors";
-
-export const cbWalletConnector = coinbaseWallet({
-  appName: "Wagmi Smart Wallet",
-  preference: "smartWalletOnly",
-});
+import { metaMask } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [baseSepolia],
   // turn off injected provider discovery
   multiInjectedProviderDiscovery: false,
-  connectors: [cbWalletConnector],
+  connectors: [metaMask()],
   ssr: true,
   transports: {
     [baseSepolia.id]: http(),
